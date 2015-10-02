408da194a61c4f6456bf2733887b5f3a151dd0a4
lucassm <lucas.silveiramelo@gmail.com>
2015-10-02 09:22:24 -0300

    retirada dos arquivos .pyc

--- pade/examples/start_ams.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.insert(1, '/home/lucas/Dropbox/workspace/Twisted_Agents/')
-
-from misc.common import set_ams, start_loop
-
-if __name__ == '__main__':
-    set_ams('localhost', 8000)
-    start_loop(list(), gui=True)
