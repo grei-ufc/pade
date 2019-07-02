139dece82c24dd41187eb4328aef57cc1d9e08f4
yeray <yeraymm@usal.es>
2019-07-02 14:38:22 +0200

    Added the Session parameter needed by the pade/misc/common file in init_database

--- pade/core/new_ams.py
+++ pade/core/new_ams.py
@@ -222,6 +222,7 @@ class CompVerifyRegister(FipaRequestProtocol):
                                              'content': validation}))
                     self.agent.send(reply)
 
+
 class AMS(Agent_):
     """This is the class that implements the AMS agent."""
 
@@ -230,9 +231,15 @@ class AMS(Agent_):
     user_login = dict()
     ams_debug = False
 
-    def __init__(self, host='localhost', port=8000, main_ams=True, debug=False):
+    def __init__(self, host='localhost', session=None, port=8000, main_ams=True, debug=False):
 
-        self.session_name = str(uuid.uuid1())[:13]
+        # Added session parameter
+        if session is None:
+            self.session_name = str(uuid.uuid1())[:13]
+        else:
+            self.session_name = session.name
+            self.session = session
+
         self.ams = {'name': host, 'port': port}
 
         self.ams_aid = AID('ams@' + str(host) + ':' + str(port))
@@ -310,6 +317,7 @@ class AMS(Agent_):
         # in case there is a session with this name
         else:
             self._verify_user_in_session(self.session)
+
 
 if __name__ == '__main__':
 
