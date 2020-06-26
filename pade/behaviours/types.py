01c7617db49198fb604be1faff2ee200a6d46bb5
Filipe Saraiva <saraiva@ufpa.br>
2020-06-26 11:29:52 -0300

    Removes `receive` method from SequentialBehaviour

--- pade/behaviours/types.py
+++ pade/behaviours/types.py
@@ -154,12 +154,3 @@ class SequentialBehaviour(OneShotBehaviour):
 		''' This method adds sub-behaviours in this behaviour
 		'''
 		self.subbehaviours.append(behaviour)
-
-	def receive(self, message):
-		''' Overridden method to pass a received message to sub-behaviours.
-		'''
-		if isinstance(message, ACLMessage):
-			for behaviour in self.subbehaviours:
-				behaviour.messages.put(message)
-		else:
-			raise ValueError('message object type must be ACLMessage!')
