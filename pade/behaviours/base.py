c465416f90d9ccf41a9245d2011ee35482392250
Filipe Saraiva <saraiva@ufpa.br>
2020-06-26 09:43:39 -0300

    PEP8: uses spaces instead tab

--- pade/behaviours/base.py
+++ pade/behaviours/base.py
@@ -8,62 +8,61 @@ from pade.acl.messages import ACLMessage
 from time import sleep
 
 class BaseBehaviour(Behaviour):
+    ''' BaseBehaviour class ihnerits from Behaviour class and
+    implements the basic methods for scheduled behaviours.
+    '''
 
-	''' BaseBehaviour class ihnerits from Behaviour class and
-	implements the basic methods for scheduled behaviours.
-	'''
+    def __init__(self, agent):
+        ''' This method initializes a new instance of BaseBehaviour
+        class and explicitly calls the Behaviour.__init__() method.
+        '''
+        super().__init__(agent)
+        # Lock object (to ensure the mutual exclusion, when needed)
+        self.__lock = None
 
-	def __init__(self, agent):
-		''' This method initializes a new instance of BaseBehaviour
-		class and explicitly calls the Behaviour.__init__() method.
-		'''
-		super().__init__(agent)
-		# Lock object (to ensure the mutual exclusion, when needed)
-		self.__lock = None
-
-	def action(self):
-		''' This is an abstract method that must be overridden in the
-		child classes, writing the main actions of this behaviour.
-		'''
-		pass
-
-
-	def done(self):
-		''' This is an abstract method that must be overridden in the
-		child classes, dealing with the finish of this behaviour.
-		'''
-		pass
-
-	def wait(self, timeout):
-		''' This method sleeps a behaviour until occurs a timeout. The
-		behaviour will execute normally afterwards.
-		'''
-		sleep(timeout)
+    def action(self):
+        ''' This is an abstract method that must be overridden in the
+        child classes, writing the main actions of this behaviour.
+        '''
+        pass
 
 
-	def on_end(self):
-		''' The scheduler will calls this method after the self.done()
-		method returns True and before the end of this behaviour. It is
-		the last action of a behaviour.
-		'''
-		pass
+    def done(self):
+        ''' This is an abstract method that must be overridden in the
+        child classes, dealing with the finish of this behaviour.
+        '''
+        pass
 
-	def add_lock(self, lock):
-		''' Adds a threading.Lock object to this behaviour, allowing 
-		it to execute the mutual exclusion correctly.
-		'''
-		self.__lock = lock
-	
+    def wait(self, timeout):
+        ''' This method sleeps a behaviour until occurs a timeout. The
+        behaviour will execute normally afterwards.
+        '''
+        sleep(timeout)
 
-	def lock(self):
-		''' Tries to acquire the lock of the threading.Lock object.
-		The behaviour will block if fails.
-		'''
-		self.__lock.acquire()
-	
 
-	def unlock(self):
-		''' Releases the lock of the threading.Lock object, allowing
-		the other behaviours to acquire it.
-		'''
-		self.__lock.release()
+    def on_end(self):
+        ''' The scheduler will calls this method after the self.done()
+        method returns True and before the end of this behaviour. It is
+        the last action of a behaviour.
+        '''
+        pass
+
+    def add_lock(self, lock):
+        ''' Adds a threading.Lock object to this behaviour, allowing 
+        it to execute the mutual exclusion correctly.
+        '''
+        self.__lock = lock
+    
+
+    def lock(self):
+        ''' Tries to acquire the lock of the threading.Lock object.
+        The behaviour will block if fails.
+        '''
+        self.__lock.acquire()
+    
+
+    def unlock(self):
+        ''' Releases the lock of the threading.Lock object, allowing
+        the other behaviours to acquire it.
+        '''
+        self.__lock.release()
