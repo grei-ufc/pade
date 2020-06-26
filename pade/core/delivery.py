3c30e8d6791846e1095ffd70dc6de6bbb395e3c5
Italo Campos <italo.ramon.campos@gmail.com>
2020-06-26 12:00:06 -0300

    Adds behaviour return feature
    [Added]
    - Adding attributes and methods in SimpleBehaviour class in order to handle behaviour return feature;
    
    [Changed]
    - PEP8: Repleacing TABS for spaces;

--- pade/core/delivery.py
+++ pade/core/delivery.py
@@ -11,27 +11,27 @@ from queue import Queue
 from pade.misc.utility import display_message
 
 class DeliverPostponedMessage(CyclicBehaviour):
-	''' DeliveryPostponedMessage class basically executes the attempt to deliver 
-	the message behaviour. It stores the messages in a queue until the max_wait
-	time is reach.
-	'''
+    ''' DeliveryPostponedMessage class basically executes the attempt to deliver 
+    the message behaviour. It stores the messages in a queue until the max_wait
+    time is reach.
+    '''
 
-	def __init__(self, agent, max_wait):
-		super().__init__(agent)
-		self.queue = Queue()
-		self.max_wait = max_wait
+    def __init__(self, agent, max_wait):
+        super().__init__(agent)
+        self.queue = Queue()
+        self.max_wait = max_wait
 
-	def action(self):
-		self.wait(0.5)
-		package = self.queue.get()
-		elapsed_time = datetime.now() - package['time_stamp']
-		if elapsed_time.total_seconds() <= self.max_wait:
-			if not self.agent.receiver_available(package['message'].receivers[0]):
-				self.queue.put(package)
-			else:
-				self.agent.send(package['message'])
-		else:
-			display_message(self.agent, 'A message could not be delivered to the receiver %s.' % package['message'].receivers[0].getName())
+    def action(self):
+        self.wait(0.5)
+        package = self.queue.get()
+        elapsed_time = datetime.now() - package['time_stamp']
+        if elapsed_time.total_seconds() <= self.max_wait:
+            if not self.agent.receiver_available(package['message'].receivers[0]):
+                self.queue.put(package)
+            else:
+                self.agent.send(package['message'])
+        else:
+            display_message(self.agent, 'A message could not be delivered to the receiver %s.' % package['message'].receivers[0].getName())
 
-	def deliver(self, message):
-		self.queue.put({'message': message,	'time_stamp': datetime.now()})
+    def deliver(self, message):
+        self.queue.put({'message': message,    'time_stamp': datetime.now()})
--- pade/scheduler/core.py
+++ pade/scheduler/core.py
@@ -11,40 +11,40 @@ from pade.misc.utility import display
 from queue import Queue
 
 class Scheduler(Thread):
-	''' Scheduler class basically executes each behaviour within a
-	thread (under BehaviourTask form, here called simply 'Task').
-	'''
+    ''' Scheduler class basically executes each behaviour within a
+    thread (under BehaviourTask form, here called simply 'Task').
+    '''
 
-	def __init__(self, agent):
-		super().__init__()
-		self.agent = agent
-		self.active_tasks = list() # Active tasks queue
-		self.tasks = Queue() # Tasks queue
+    def __init__(self, agent):
+        super().__init__()
+        self.agent = agent
+        self.active_tasks = list() # Active tasks queue
+        self.tasks = Queue() # Tasks queue
 
-	def run(self):
-		''' It executes all scheduled tasks in the 'tasks' queue.
-		'''
-		while self.agent.active:
-			task = self.tasks.get()
-			task.start()
+    def run(self):
+        ''' It executes all scheduled tasks in the 'tasks' queue.
+        '''
+        while self.agent.active:
+            task = self.tasks.get()
+            task.start()
 
 
 class BehaviourTask(Thread):
-	''' The Task class manages each behaviour separately in an one thread,
-	deciding whether a behaviour must goes to active queue or to blocked
-	queue. Each instance of this class runs a behaviour once only.
-	'''
-	def __init__(self, behaviour, scheduler):
-		super().__init__()
-		self.behaviour = behaviour
-		self.scheduler = scheduler
+    ''' The Task class manages each behaviour separately in an one thread,
+    deciding whether a behaviour must goes to active queue or to blocked
+    queue. Each instance of this class runs a behaviour once only.
+    '''
+    def __init__(self, behaviour, scheduler):
+        super().__init__()
+        self.behaviour = behaviour
+        self.scheduler = scheduler
 
-	def run(self):
-		''' This method executes the BaseBehaviour.action() method, until
-		it returns False (i.e, until its end).
-		'''
-		self.behaviour.action() # Execute the action() method at least once.
-		while not self.behaviour.done():
-			self.behaviour.action() # Execute the behaviour actions
-		self.behaviour.on_end() # Lasts actions of the behaviour
-		self.scheduler.agent.remove_task(self) # Thread will die after its behaviour execution 
+    def run(self):
+        ''' This method executes the BaseBehaviour.action() method, until
+        it returns False (i.e, until its end).
+        '''
+        self.behaviour.action() # Execute the action() method at least once.
+        while not self.behaviour.done():
+            self.behaviour.action() # Execute the behaviour actions
+        self.behaviour.on_end() # Lasts actions of the behaviour
+        self.scheduler.agent.remove_task(self) # Thread will die after its behaviour execution 
